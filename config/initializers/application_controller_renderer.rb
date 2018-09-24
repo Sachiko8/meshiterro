4da8df03bcbaaf102bf4c02a0371c1c54a7dd9b8
Sachiko Suzuki <sachiko@Sachiko.local>
2018-09-24 20:18:25 +0900

    AWS

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'c44a397169c8b819567b15d5bfdce4614e24040eb9d881ef1cd97e2517d32c802c922ea00d88c5289bd8a7db4d2109eb1d248f1fcb5ceecfea08346501f51ff6'
